fc2dfc6def3ef594fc8ed22327901ec6b83afc1d
Caitlin <caitlin.sigrist@gmail.com>
2021-05-07 16:59:08 +0200

    chore: shifting of sockets' logic on socket.handler

---
+++ app/services/socket.handler.js
@@ -0,0 +1,53 @@
+const socketHandler = {
+    auth: socket => {
+        socket.on('auth', ({ channel/*, user*/ }) => {
+            /*
+            {
+                user : {
+                    id : number,
+                    nickname : string
+                }
+                channel : {
+                    id : number
+                }
+            }
+            */
+            console.log('auth ok')
+            socket.join(`channel-${channel.id}`);
+
+            socket.emit('confirm')
+
+            // key 'user-join' to tell front to add user to user list
+            // io.to(`channel-${message.channel.id}`).emit('user-join', {channel, user})
+            // Channel.addUser(User.findByPk(user.id))
+        })
+    },
+
+    message: (socket, messageIndex) => {
+        socket.on('message', message => {
+            /*
+            {
+                id : string (messageId) (have to be send)
+                user : {
+                    id : number,
+                    nickname : string
+                }
+                channel : {
+                    id : number
+                },
+                content : string
+            }
+            */
+            message.id = messageIndex++
+            console.log('message ok', messageIndex)
+            io.to(`channel-${message.channel.id}`).emit('message', message);
+        })
+    },
+
+    // socket.on('disconnect', ({ channel, user }) => {
+    //     // key 'user-join' to tell front to add user to user list
+    //     io.to(`channel-${channel.id}`).emit('user-leave', {channel, user});
+    // })
+};
+
+module.exports = socketHandler;
--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const io = new Server(httpServer, {
 });
 
 const apiRouter = require('./app/router');
+const socketHandler = require('./app/services/socket.handler');
 
 const PORT = process.env.PORT || 8000;
 
@@ -29,51 +30,8 @@ app.use('/v1', apiRouter);
 let messageIndex = 0;
 
 io.on('connection', socket => {
-    socket.on('auth', ({ channel/*, user*/ }) => {
-        /*
-        {
-            user : {
-                id : number,
-                nickname : string
-            }
-            channel : {
-                id : number
-            }
-        }
-        */
-
-        socket.join(`channel-${channel.id}`);
-
-        // socket.emit('confirm')
-
-        // key 'user-join' to tell front to add user to user list
-        // io.to(`channel-${message.channel.id}`).emit('user-join', {channel, user})
-        // Channel.addUser(User.findByPk(user.id))
-    })
-
-    socket.on('message', message => {
-        /*
-        {
-            id : string (messageId) (have to be send)
-            user : {
-                id : number,
-                nickname : string
-            }
-            channel : {
-                id : number
-            },
-            content : string
-        }
-        */
-        message.id = messageIndex++
-
-        io.to(`channel-${message.channel.id}`).emit('message', message);
-    })
-
-    // socket.on('disconnect', ({ channel, user }) => {
-    //     // key 'user-join' to tell front to add user to user list
-    //     io.to(`channel-${channel.id}`).emit('user-leave', {channel, user});
-    // })
+    socketHandler.auth(socket);
+    socketHandler.message(socket, messageIndex)
 })
 
 httpServer.listen(PORT, () => console.log(`Serveur running on http://localhost:${PORT}`));
