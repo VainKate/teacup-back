221172b23a212a26b1006622bad1813d41e95add
Caitlin <caitlin.sigrist@gmail.com>
2021-05-09 11:54:41 +0200

    chore: consistant seeding done

--- data/db-generate.js
+++ data/db-generate.js
@@ -1,4 +1,5 @@
 const { sequelize, Channel, Tag, User } = require('../app/models');
+const { Op } = require('sequelize')
 const bcrypt = require('bcrypt');
 const faker = require('faker')
 
@@ -13,67 +14,281 @@ const SALT_ROUNDS = 10;
     try {
         await sequelize.sync({ force: true })
 
-        await new Tag({ name: "Cuisine" }).save();
-        await new Tag({ name: "Horreur" }).save();
-        await new Tag({ name: "Variété française" }).save();
-        await new Tag({ name: "Catch" }).save();
-        await new Tag({ name: "Planche à voile" }).save();
-        await new Tag({ name: "Film" }).save();
-        await new Tag({ name: "Comédie musicales" }).save();
-        await new Tag({ name: "Mangas/Animes" }).save();
-        await new Tag({ name: "Poterie" }).save();
-        await new Tag({ name: "Jeux Vidéos" }).save();
-        await new Tag({ name: "Action" }).save();
-        await new Tag({ name: "Jeux de société" }).save();
-        await new Tag({ name: "Romantique" }).save();
-        await new Tag({ name: "Séries TV" }).save();
+        await Tag.create({ name: "Cuisine" });
+        await Tag.create({ name: "Horreur" });
+        await Tag.create({ name: "Variété française" });
+        await Tag.create({ name: "Catch" });
+        await Tag.create({ name: "Planche à voile" });
+        await Tag.create({ name: "Films" });
+        await Tag.create({ name: "Comédies Musicales" });
+        await Tag.create({ name: "Mangas/Animes" });
+        await Tag.create({ name: "Poterie" });
+        await Tag.create({ name: "Jeux Vidéos" });
+        await Tag.create({ name: "Action" });
+        await Tag.create({ name: "Jeux de cartes" });
+        await Tag.create({ name: "Jeux de société" });
+        await Tag.create({ name: "Romantique" });
+        await Tag.create({ name: "Séries TV" });
+        await Tag.create({ name: "Mignon" });
+        await Tag.create({ name: "Épique" });
+        await Tag.create({ name: "RPG" });
+        await Tag.create({ name: "Livres" });
+        await Tag.create({ name: "Stratégie" });
+        await Tag.create({ name: "Comédie" });
+        await Tag.create({ name: "Mystère" });
+        await Tag.create({ name: "Réflexion" });
+        await Tag.create({ name: "Enquête" });
+        await Tag.create({ name: "Sport" });
+        await Tag.create({ name: "Comics" });
 
-        for (let index = 0; index < 35; index++) {
-            const newUser = await new User({
-                email: faker.internet.email(),
-                password: await bcrypt.hash(faker.internet.password(), SALT_ROUNDS),
-                nickname: faker.internet.userName()
-            }).save()
+        const chan1 = await Channel.create({ title: "GLOW" });
+        await chan1.reload();
+        await chan1.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Séries TV", "Catch", "Comédie"]
+                }
+            }
+        }));
 
-            await newUser.reload()
+        const chan2 = await Channel.create({ title: "The walking dead" });
+        await chan2.reload();
+        await chan2.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Séries TV", "Horreur", "Action", "Comics"]
+                }
+            }
+        }));
 
-            await newUser.addTag(await Tag.findAll({
-                order: sequelize.random(),
-                limit: Math.round(Math.random() * (11 - 1) +1 )
-            }))
-        }
+        const chan3 = await Channel.create({ title: "Jeux vidéos d'action" });
+        await chan3.reload();
+        await chan3.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux Vidéos", "Action"]
+                }
+            }
+        }));
+
+        const chan4 = await Channel.create({ title: "Films d'action" });
+        await chan4.reload();
+        await chan4.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Films", "Action"]
+                }
+            }
+        }));
+
+        const chan5 = await Channel.create({ title: "Cats" });
+        await chan5.reload();
+        await chan5.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Films", "Comédies Musicales"]
+                }
+            }
+        }));
+
+        const chan6 = await Channel.create({ title: "School Live/Gakkougurashi" });
+        await chan6.reload();
+        await chan6.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Mignon", "Mangas/Animes", "Horreur"]
+                }
+            }
+        }));
+
+        const chan7 = await Channel.create({ title: "Higurashi no naku koro ni" });
+        await chan7.reload();
+        await chan7.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Mignon", "Mangas/Animes", "Horreur", "Mystère"]
+                }
+            }
+        }));
+
+        const chan8 = await Channel.create({ title: "The Witcher" });
+        await chan8.reload();
+        await chan8.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux Vidéos", "Séries TV", "Épique", "Action", "RPG", "Livres"]
+                }
+            }
+        }));
+
+        const chan9 = await Channel.create({ title: "Magic" });
+        await chan9.reload();
+        await chan9.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux de cartes", "Stratégie", "Réflexion"]
+                }
+            }
+        }));
+
+        const chan10 = await Channel.create({ title: "Let's create! Pottery VR" });
+        await chan10.reload();
+        await chan10.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux Vidéos", "Poterie"]
+                }
+            }
+        }));
+
+        const chan11 = await Channel.create({ title: "Danganronpa" });
+        await chan11.reload();
+        await chan11.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux Vidéos", "Mangas/Animes", "Horreur", "Enquête", "Réflexion"]
+                }
+            }
+        }));
+
+        const chan12 = await Channel.create({ title: "Crazy Ex-Girlfriend" });
+        await chan12.reload();
+        await chan12.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Série TV", "Comédies Musicales", "Comédie", "Romantique"]
+                }
+            }
+        }));
+
+        const chan13 = await Channel.create({ title: "Dixit" });
+        await chan13.reload();
+        await chan13.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux de société", "Réflexion"]
+                }
+            }
+        }));
+
+        const chan14 = await Channel.create({ title: "Fiesta de los Muertos" });
+        await chan14.reload();
+        await chan14.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux de société", "Réflexion"]
+                }
+            }
+        }));
+
+        const chan15 = await Channel.create({ title: "Muertigos" });
+        await chan15.reload();
+        await chan15.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux de société", "Réflexion", "Jeux Vidéos"]
+                }
+            }
+        }));
+
+        const chan16 = await Channel.create({ title: "Portal" });
+        await chan16.reload();
+        await chan16.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Réflexion", "Jeux Vidéos"]
+                }
+            }
+        }));
+
+        const chan17 = await Channel.create({ title: "Les pires horreurs en cuisine" });
+        await chan17.reload();
+        await chan17.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Cuisine", "Horreur"]
+                }
+            }
+        }));
+
+        const chan18 = await Channel.create({ title: "Le meilleur du catch à coup de planche à voile !" });
+        await chan18.reload();
+        await chan18.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Catch", "Planche à voile", "Sport"]
+                }
+            }
+        }));
+
+        const chan19 = await Channel.create({ title: "Phoenix Wright/Ace Attorney" });
+        await chan19.reload();
+        await chan19.addTag(await Tag.findAll({
+            where: {
+                name: {
+                    [Op.or]: ["Jeux Vidéos", "Mangas/Animes", "Épique", "Enquête", "Réflexion"]
+                }
+            }
+        }));
+
 
         for (const tag of await Tag.findAll()) {
-            const newChannel = await new Channel({ title: tag.name }).save()
-            newChannel.reload()
+            const newChannel = await Channel.create({ title: tag.name });
+            await newChannel.reload();
 
             await newChannel.addTag(tag);
         }
 
-        const channel1 = await new Channel({ title: 'Les griffes de la nuit' }).save();
+        for (let index = 0; index < 35; index++) {
+            const newUser = await User.create({
+                email: faker.internet.email(),
+                password: await bcrypt.hash(faker.internet.password(), SALT_ROUNDS),
+                nickname: faker.internet.userName()
+            });
 
-        // const tag1 = new Tag({ name: 'Film' });
-        // await tag1.save();
-        // const updatedTag1 = await tag1.reload()
+            await newUser.reload();
 
-        // const tag2 = new Tag({ name: 'Horreur' })
-        // await tag2.save();
-        // const updatedTag2 = await tag2.reload()
+            await newUser.addTag(await Tag.findAll({
+                order: sequelize.random(),
+                limit: Math.round(Math.random() * (11 - 1) + 1)
+            }));
 
-        // await channel1.addTag(updatedTag1);
-        // await channel1.addTag(updatedTag2);
+            const userTags = await newUser.getTags();
 
-        // const user2 = new User({
-        //     email: "bidule@gmail.com",
-        //     password: await bcrypt.hash("tOuJoUrSpAsLeNoMdEmOnChIeN", SALT_ROUNDS),
-        //     nickname: "belgoss-du-67"
-        // });
-        // await user2.save()
+            await newUser.addChannel(await Channel.findAll({
+                include: {
+                    association: "tags",
+                    through: {
+                        attributes: [],
+                    },
+                    where: {
+                        id: userTags.map(({ id }) => id)
+                    }
+                },
+                order: sequelize.random(),
+                limit: Math.round(Math.random() * (userTags.length - 3) + 3)
+            }));
 
-        // await user1.addTag(updatedTag1);
-        // await user2.addTag(updatedTag2);
+            const userChannels = await newUser.getChannels()
 
-        // await channel1.addUser(updatedUser1);
+            await newUser.addChannel(await Channel.findAll({
+                include: {
+                    association: "tags",
+                    through: {
+                        attributes: [],
+                    },
+                    where: {
+                        id: 
+                        {
+                            [Op.not]: userTags.map(({ id }) => id)
+                        }
+                    }
+                },
+                order: sequelize.random(),
+                limit: Math.round(Math.random() * userChannels.length/3)
+            }));
+        }
+
     }
 
     catch (err) {
