e079af67c4e18bcdd72dfe31e5d836fa08412a3e
Caitlin <caitlin.sigrist@gmail.com>
2021-05-10 15:44:38 +0200

    wip: channels endpoint

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const { authController, channelController, tagController } = require("./controll
 // [post] route for signup registration
 router.post("/signup", authController.signup);
 
+router.get('/channels', channelController.getAllChannels);
 router.get('/channel/:id(\\d+)', channelController.getChannelById);
 
 router.get('/tags', tagController.getAllTags);
