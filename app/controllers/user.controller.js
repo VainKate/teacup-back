a490657cba045604c0e66c432c59990eccf66bb3
Guillaume67450 <guillaume_d_mundo@hotmail.com>
2021-05-14 11:24:16 +0200

    update tags

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -8,19 +8,39 @@ const userController = {
      * @returns
      */
 
-    update: async (req, res, next) => {
-        const id = parseInt(req.params.id);
+    update: async (req, res) => {
+        const id = req.params.id;
+        const {email, nickname, tags} = req.body;
 
         try {
-            const result = await User.update(req.body, {where: {id}});
-            if (result[0] >= 1) {
-                res.json(result);
-            } else {
-                next();
-            }
+            const options = tags ? {
+                include : {
+                    association : "tags",
+                    through : {
+                        attributes : []
+                    }
+                }
+            } : null
+
+            const user = await User.findByPk(id, options);
+
+            if(!user){
+                return res.status(400).send('No user found.')
+            };
+
+            await user.update({email, nickname});
+
+            if(tags){
+            await user.setTags(tags);
+            };
+            
+            await user.reload()
+
+            res.json(user)
+
         } catch(error) {
-            console.error(error);
             res.status(500).json({error: error.message});
+
         }
     },
 }
