1683711e54708b1d42c254cc31b8cc410642ec27
AngelZatch <angelzatch@gmail.com>
2021-07-01 18:07:06 +0200

    fix: use nanoid
    crypto fails

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto')
+const { nanoid } = require('nanoid');
 const asyncClient = require('../redisClient');
 
 
@@ -59,7 +59,7 @@ const auth = {
     },
 
     generateResetKey: async (payload, success) => {
-        const resetKey = crypto.randomUUID();
+        const resetKey = nanoid();
 
         if (success) {
             await asyncClient.setex(`${PREFIX}resetPasswordKey-email${payload.email}`,
